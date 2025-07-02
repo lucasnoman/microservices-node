afeb8b6f53dd53d31fcfdd905d91bd25b1a3ae91
Lucas C. Noman <lucas.noman@outlook.com>
2025-07-02 20:57:01 -0300

    feat(infra): implement Kong service with custom entrypoint and configuration template

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,11 @@
 import * as pulumi from '@pulumi/pulumi'
 
 import { appLoadBalancer } from './src/load-balancer'
+import { kongService } from './src/services/kong'
 import { ordersService } from './src/services/orders'
 import { rabbitMQService } from './src/services/rabbitmq'
 
 export const ordersId = ordersService.service.id
 export const rabbitMQId = rabbitMQService.service.id
+export const kongId = kongService.service.id
 export const rabbitMQAdminUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`
