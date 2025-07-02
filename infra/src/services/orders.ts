6fd383f76716cfd0ca67d1152b10ab5f94d5244e
Lucas C. Noman <lucas.noman@outlook.com>
2025-07-02 20:19:08 -0300

    refactor(infra): updated database connection to use Neon (cloud) for "Orders" and configured OpenTelemetry traces on Grafana cloud

--- infra/src/services/orders.ts
+++ infra/src/services/orders.ts
@@ -3,7 +3,26 @@ import * as pulumi from '@pulumi/pulumi'
 
 import { cluster } from '../cluster'
 import { ordersDockerImage } from '../images/orders'
+import { appLoadBalancer } from '../load-balancer'
 import { amqpListener } from './rabbitmq'
+
+const ordersTargetGroup = appLoadBalancer.createTargetGroup('orders-target', {
+  port: 3333,
+  protocol: 'HTTP',
+  healthCheck: {
+    path: '/health',
+    protocol: 'HTTP',
+  },
+})
+
+export const ordersHttpListener = appLoadBalancer.createListener(
+  'orders-listener',
+  {
+    port: 3333,
+    protocol: 'HTTP',
+    targetGroup: ordersTargetGroup,
+  }
+)
 
 // The creation of this ECT with `awsx` automatically enables a Lifecycle Policy, removing older untagged images. This reduces costs and keeps the repository clean.
 export const ordersService = new awsx.classic.ecs.FargateService(
@@ -17,6 +36,7 @@ export const ordersService = new awsx.classic.ecs.FargateService(
         image: ordersDockerImage.ref,
         cpu: 256,
         memory: 512,
+        portMappings: [ordersHttpListener],
         environment: [
           {
             name: 'BROKER_URL',
@@ -26,6 +46,36 @@ export const ordersService = new awsx.classic.ecs.FargateService(
             name: 'DATABASE_URL',
             value:
               'postgresql://neondb_owner:npg_jwZCatyQ0iM2@ep-rapid-base-aexilvgo.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+          },
+          {
+            name: 'OTEL_TRACES_EXPORTER',
+            value: 'otlp',
+          },
+          {
+            name: 'OTEL_EXPORTER_OTLP_ENDPOINT',
+            value: 'https://otlp-gateway-prod-sa-east-1.grafana.net/otlp',
+          },
+          {
+            name: 'OTEL_EXPORTER_OTLP_HEADERS',
+            value:
+              'Authorization=Basic MTAxNDMxMzpnbGNfZXlKdklqb2lNVEU1T1RNek15SXNJbTRpT2lKbGRtVnVkRzh0Ym05a1pXcHpJaXdpYXlJNklsZFpSR2h1TVdJME1EUkZNamcxUnpKaFNubHJNVFoxU1NJc0ltMGlPbnNpY2lJNkluQnliMlF0YzJFdFpXRnpkQzB4SW4xOQ==',
+          },
+          {
+            name: 'OTEL_SERVICE_NAME',
+            value: 'orders',
+          },
+          {
+            name: 'OTEL_RESOURCE_ATTRIBUTES',
+            value:
+              'service.name=my-app,service.namespace=my-application-group,deployment.environment=production',
+          },
+          {
+            name: 'OTEL_NODE_RESOURCE_DETECTORS',
+            value: 'env,host,os',
+          },
+          {
+            name: 'OTEL_NODE_ENABLED_INSTRUMENTATIONS',
+            value: 'http,fastify,pg,amqplib',
           },
         ],
       },
